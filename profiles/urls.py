88eba35ce10e0b2d22e174ebe63c7bc38cb08e2c
Vitaliy <sprinter-100m@ukr.net>
2023-09-16 14:29:11 +0300

    adding home page

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
+    path('', views.WelkomePage.as_view(), name='home'),
     path('all/', views.UserProfileView.as_view(), name='profile-list-view'),
     path('<int:pk>/', views.UserProfileDetailView.as_view(), name='profile-detail-view'),
     path('public-profile/<str:username>/', views.PublicProfileView.as_view(), name='public-profile'),
