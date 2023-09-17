0a0d3471acc501eea04e42f3b801f0c061d81e84
Vitaliy <sprinter-100m@ukr.net>
2023-09-17 14:53:12 +0300

    profile development

--- profiles/views.py
+++ profiles/views.py
@@ -1,68 +1,25 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404
 from django.views.generic import ListView, DetailView, View
 from .models import UserProfile
-from django.contrib import messages
-from .forms import UserUpdateForm, ProfileUpdateForm
 
 
-class WelkomePage(ListView):
+class WelcomePageView(View):
     # Start page
-    model = UserProfile
-    template_name = 'profiles/welcome_page.html'
-
-
-class ProfileView(View):
-    # User profile
     def get(self, request):
-        u_form = UserUpdateForm(instance=request.user)
-        p_form = ProfileUpdateForm(instance=request.user.userprofile)
-    
-        context = {
-            'u_form': u_form,
-            'p_form': p_form
-        }
+        return render(request, 'profiles/welcome_page.html')
 
-        return render(request, 'profiles/profile.html', context)
 
-    def post(self, request):
-        u_form = UserUpdateForm(request.POST, instance=request.user)
-        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+class UserProfilelView(ListView):
+    # User profile details view
+    model = UserProfile
+    template_name = 'profiles/userprofile_list.html'
 
-        if u_form.is_valid() and p_form.is_valid():
-            u_form.save()
-            p_form.save()
-            messages.success(request, f"Ваша учетная запись обновлена!")
-            return redirect('profiles/profile.html')
-
-        context = {
-            'u_form': u_form,
-            'p_form': p_form
-        }
-
-        return render(request, 'profiles/profile.html', context)
+    def get_queryset(self):
+        return UserProfile.objects.all().exclude(user=self.request.user)
 
 
 class PublicProfileView(View):
     # Creating a public profile view
     def get(self, request, username):
-        user = UserProfile.objects.get(username=username)
+        user = get_object_or_404(UserProfile, username=username)
         return render(request, 'profiles/public_profile.html', {"context": user})
-
-
-class UserProfileView(ListView):
-    # All user profiles
-    model = UserProfile
-    template_name = "profiles/all_profiles.html"
-    context_object_name = "profiles"
-    
-    def get_queryset(self):
-        return UserProfile.objects.all().exclude(user=self.request.user)
-    
-
-class UserProfileDetailView(DetailView):
-    # User profile details view
-    model = UserProfile
-    template_name = 'profiles/userprofile_dateil.html'
-
-    def get_queryset(self):
-        return UserProfile.objects.all().exclude(user=self.request.user)
