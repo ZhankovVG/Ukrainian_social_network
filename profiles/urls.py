839bb5adf677defe51d4ffda667d7b65d4e19514
Vitaliy <sprinter-100m@ukr.net>
2023-09-18 19:19:18 +0300

    profile development

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
     path('', views.WelcomePageView.as_view(), name='welcome_page'),
-    path('profiles/<int:pk>/', views.UserProfilelView.as_view(), name='profile_detail_view'),
-    path('public_profile/<int:pk>/', views.PublicProfileView.as_view(), name='public_profile'),   
+    path('all/', views.ProfileListAllView.as_view(), name='profile_list_view'),
+    path('profiles/<int:pk>/', views.ProfileDetailView.as_view(), name='profile_detail_view'),   
+    path('public_profile/<str:username>/', views.PublicProfileView.as_view(), name='public_profile'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render, get_object_or_404
 from django.views.generic import ListView, DetailView, View
 from .models import UserProfile
+from django.contrib.auth.models import User
 
 
 class WelcomePageView(View):
@@ -9,8 +10,8 @@ class WelcomePageView(View):
         return render(request, 'profiles/welcome_page.html')
 
 
-class UserProfilelView(ListView):
-    # User profile details view
+class ProfileListAllView(ListView):
+    # All user profiles
     model = UserProfile
     template_name = 'profiles/userprofile_list.html'
 
@@ -18,8 +19,22 @@ class UserProfilelView(ListView):
         return UserProfile.objects.all().exclude(user=self.request.user)
 
 
+class ProfileDetailView(DetailView):
+    # Creating a public profile view
+    model = UserProfile
+    template_name = 'profiles/profile_details.html'
+    context_object_name = 'user'
+
+    def get_queryset(self):
+        return UserProfile.objects.all().exclude(user=self.request.user)
+
+    def get_object(self, **kwargs):
+        pk = self.kwargs.get("pk")
+        return get_object_or_404(UserProfile, pk=pk)
+    
+
 class PublicProfileView(View):
     # Creating a public profile view
     def get(self, request, username):
-        user = get_object_or_404(UserProfile, username=username)
-        return render(request, 'profiles/public_profile.html', {"context": user})
+        user = get_object_or_404(User, username=username)
+        return render(request, 'profiles/public_profile.html', {"context" : user})
