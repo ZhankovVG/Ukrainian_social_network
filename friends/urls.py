e77904e5c93232bd4c21971bdc5f93c0acdb5932
Vitaliy <sprinter-100m@ukr.net>
2023-10-03 19:07:06 +0300

    development adding friends

--- friends/urls.py
+++ friends/urls.py
@@ -6,4 +6,5 @@ app_name = "friends"
 urlpatterns = [
     path('find_friends/', views.FindFriendsListView.as_view(), name="find_friends"),
     path('friend_requests/', views.FriendRequestsListView.as_view(), name="friend_requests"),
+    path('send_friend_request/<int:user_id>/', views.SendFriendshipRequestView.as_view(), name='send_friend_request'),
 ]
--- friends/views.py
+++ friends/views.py
@@ -4,21 +4,29 @@ from profiles.models import Profile
 from django.views.generic import View, ListView
 from django.db.models import Q
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import get_object_or_404
+from django.contrib import messages
 
 
 class FindFriendsListView(LoginRequiredMixin, ListView):
+    # Search friends
     model = Friend
     context_object_name = 'users'
     template_name = "friends/find_friends.html"
 
     def get_queryset(self):
+        search_query = self.request.GET.get('search')
         current_user_friends = self.request.user.friends.values('id')
         sent_request = list(
             FriendshipRequest.objects.filter(Q(from_user=self.request.user))
-            .exclude(to_user_id=self.request.user.id)
+            .exclude(to_user=self.request.user.id)
             .values_list('to_user_id', flat=True))
+
         users = Profile.objects.exclude(id__in=current_user_friends).exclude(id__in=sent_request).exclude(
             id=self.request.user.id)
+        if search_query:
+            users = users.filter(username__icontains=search_query)
+
         return users
      
         
@@ -30,3 +38,32 @@ class FriendRequestsListView(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
         return Friend.objects.got_friend_requests(user=self.request.user)
+    
+
+class SendFriendshipRequestView(View):
+    def post(self, request, user_id):
+        target_user = get_object_or_404(Profile, id=user_id)
+
+        existing_request = FriendshipRequest.objects.filter(
+            from_user=request.user,
+            to_user=target_user,
+            is_active=True
+        ).first()
+
+        if existing_request:
+            messages.error(
+                request,
+                "Ви вже надіслали запит на дружбу цьому користувачу."
+            )
+        else:
+            FriendshipRequest.objects.create(
+                from_user=request.user,
+                to_user=target_user,
+                is_active=True
+            )
+            messages.success(
+                request,
+                "Запит на дружбу надіслано успішно."
+            )
+
+        return redirect('welcome_page', user_id=user_id)
