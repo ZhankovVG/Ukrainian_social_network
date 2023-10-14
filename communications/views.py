652f04e7f3454fa6331550a80ba0757144f79f65
Vitaliy <sprinter-100m@ukr.net>
2023-10-14 13:29:22 +0300

    development of communication and minor code corrections

--- communications/views.py
+++ communications/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy as _
+from django.utils.safestring import mark_safe
+
+from profiles.models import Profile
+from friends.models import Friend
+
+
+@login_required(login_url=_("accounts:login"))
+def all_messages(request):
+    friends = Friend.objects.friends(request.user)
+    return render(request, "communications/all-messages.html", {'friends': friends})
+
+
+# Conversation with one friend
+@login_required(login_url=_("accounts:login"))
+def messages_with_one_friend(request, friend):
+    if request.user.username == friend:
+        return redirect(_('communications:all-messages'))
+    try:
+        if not Profile.objects.get(username=friend):
+            return redirect(_('communications:all-messages'))
+    except:
+        return redirect(_('communications:all-messages'))
+    friend_user = Profile.objects.get(username=friend)
+    if not Friend.objects.are_friends(request.user, friend_user):
+        return redirect(_('communications:all-messages'))
+    friends = Friend.objects.friends(request.user)
+    return render(request, "communications/friend-messages.html", {
+        'friends': friends,
+        'friend_user': friend_user,
+        'friend_name_json': mark_safe(json.dumps(friend)),
+        'username': mark_safe(json.dumps(request.user.username)),
+    })
--- profiles/urls.py
+++ profiles/urls.py
@@ -5,6 +5,6 @@ app_name = "profile"
 
 urlpatterns = [
     path('', views.WelcomePageView.as_view(), name='welcome_page'),  
-    path('public_profile/<str:username>/', views.PublicProfileView.as_view(), name='public_profile'),
+    path('public_profile/<int:pk>/', views.PublicProfileView.as_view(), name='public_profile'),
     path('edit_profile/', views.ProfileEditView, name="edit_profile"),
 ]
