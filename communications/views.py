429beb193c5147d9b4058ed6c9967b6fa200cf5b
Vitaliy <sprinter-100m@ukr.net>
2023-10-16 16:20:49 +0300

    development of communication

--- communications/views.py
+++ communications/views.py
@@ -1,36 +1,31 @@
-import json
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.urls import reverse_lazy as _
-from django.utils.safestring import mark_safe
-
-from profiles.models import Profile
 from friends.models import Friend
+from .models import Message, Room
+from django.contrib.auth import get_user_model
+User = get_user_model()
+from django.shortcuts import get_object_or_404
 
 
 @login_required(login_url=_("accounts:login"))
 def all_messages(request):
+    # Output all messages
     friends = Friend.objects.friends(request.user)
     return render(request, "communications/all-messages.html", {'friends': friends})
 
 
-# Conversation with one friend
 @login_required(login_url=_("accounts:login"))
-def messages_with_one_friend(request, friend):
-    if request.user.username == friend:
-        return redirect(_('communications:all-messages'))
-    try:
-        if not Profile.objects.get(username=friend):
-            return redirect(_('communications:all-messages'))
-    except:
-        return redirect(_('communications:all-messages'))
-    friend_user = Profile.objects.get(username=friend)
-    if not Friend.objects.are_friends(request.user, friend_user):
-        return redirect(_('communications:all-messages'))
-    friends = Friend.objects.friends(request.user)
-    return render(request, "communications/friend-messages.html", {
-        'friends': friends,
-        'friend_user': friend_user,
-        'friend_name_json': mark_safe(json.dumps(friend)),
-        'username': mark_safe(json.dumps(request.user.username)),
-    })
+def chat_with_friend(request, friend_id):
+    # Texting a friend
+    friend = get_object_or_404(User, id=friend_id)
+    room, created = Room.objects.get_or_create(author=request.user, friend=friend)
+
+    if request.method == 'POST':
+        message_text = request.POST.get('message', '')
+        if message_text:
+            message = Message(room=room, author=request.user, friend=room.friend, message=message_text)
+            message.save()
+
+    messages = Message.objects.filter(room=room)
+    return render(request, 'communications/friend_messages.html', {'room': room, 'messages': messages})
