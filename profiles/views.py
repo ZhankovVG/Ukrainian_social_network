bc26f84328e3f915b3bbc5a5d8aae02dce8b67ba
Vitaliy <sprinter-100m@ukr.net>
2023-11-05 13:30:23 +0200

    the usual bug fixes

--- profiles/views.py
+++ profiles/views.py
@@ -11,20 +11,21 @@ from django.contrib.auth.signals import user_logged_in, user_logged_out
 
 
 @receiver(user_logged_in)
-def got_online(sender, user, request, **kwargs):    
-    user.profile.is_online = True
-    user.profile.save()
+def got_online(sender, user, request, **kwargs):
+    if hasattr(user, 'profile'):
+        user.profile.is_online = True
+        user.profile.save()
 
 @receiver(user_logged_out)
-def got_offline(sender, user, request, **kwargs):   
-    user.profile.is_online = False
-    user.profile.save()
+def got_offline(sender, user, request, **kwargs):
+    if hasattr(user, 'profile'):
+        user.profile.is_online = False
+        user.profile.save()
 
 
 class WelcomePageView(View):
     @method_decorator(login_required)
     def get(self, request):
-      
         return render(request, 'profiles/welcome_page.html')
 
 
