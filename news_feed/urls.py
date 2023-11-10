ba3ada2b233a0942e32a486bd1a7ea40dbf6274b
Vitaliy <sprinter-100m@ukr.net>
2023-11-10 11:32:46 +0200

    addeng delete a post

--- news_feed/urls.py
+++ news_feed/urls.py
@@ -6,4 +6,5 @@ app_name = "newsfeed"
 urlpatterns = [
     path('post/create/', views.PostCreateView.as_view(), name='post_create'),
     path('news/', views.AllPostView.as_view(), name='news'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='delete_post'),
 ]
--- news_feed/views.py
+++ news_feed/views.py
@@ -1,13 +1,16 @@
-from django.shortcuts import render
-from django.views.generic import CreateView, ListView
+from django.shortcuts import render, redirect
+from django.views.generic import CreateView, ListView, DeleteView
 from .models import Post
 from .forms import PostCreateForm
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
 
 
 class PostCreateView(CreateView):
+    # Create new post
     model = Post
     form_class = PostCreateForm
     template_name = 'posts/post_create.html'
@@ -29,9 +32,27 @@ class PostCreateView(CreateView):
     def get_success_url(self):
         user_id = self.request.user.id 
         return reverse('profile:public_profile', kwargs={'pk': user_id})
-
-
+    
+    
 class AllPostView(ListView):
+    # Output all news
     model = Post
     template_name = 'posts/news.html'
     context_object_name = 'posts'
+
+
+class PostDeleteView(DeleteView):
+    # Delete post
+    model = Post
+    success_url = reverse_lazy('newsfeed:news')
+
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+        if obj.author != self.request.user:
+            raise PermissionError
+        return obj
+    
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        self.object.delete()
+        return redirect(self.get_success_url())
