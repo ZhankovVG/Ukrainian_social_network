75da371a7b37a03b6b6af9e2083a2db31fe33643
Vitaliy <sprinter-100m@ukr.net>
2023-11-23 15:28:56 +0200

    create comments on the posts

--- news_feed/views.py
+++ news_feed/views.py
@@ -6,7 +6,7 @@ from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 from django.urls import reverse_lazy
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponseNotFound
 from django.shortcuts import get_object_or_404
 
 
@@ -16,25 +16,25 @@ class Mixin():
         if obj.author != self.request.user:
             raise PermissionError
         return obj
-    
+
 
 class PostCreateView(CreateView):
     # Create new post
     model = Post
     form_class = PostCreateForm
     template_name = 'posts/post_create.html'
-    success_url = '/' 
+    success_url = '/'
 
     def form_valid(self, form):
         if self.request.user.is_authenticated:
             form.instance.user = self.request.user
         return super(PostCreateView, self).form_valid(form)
-        
+
     def get_success_url(self):
-        user_id = self.request.user.id 
+        user_id = self.request.user.id
         return reverse('profile:public_profile', kwargs={'pk': user_id})
-    
-    
+
+
 class AllPostView(ListView):
     # Output all news
     model = Post
@@ -63,7 +63,7 @@ class PostUpdateView(Mixin, UpdateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
-    
+
 
 class LikePostView(View):
     # Likes on posts
@@ -85,18 +85,21 @@ class LikePostView(View):
 
 
 class CommentsCreateView(View):
-    @method_decorator(login_required)
-    def post(self, request, *args, **kwakgs):
+    def get_success_url(self):
+        user_id = self.request.user.id
+        return reverse('profile:public_profile', kwargs={'pk': user_id})
+
+    def post(self, request, *args, **kwargs):
         post_id = request.POST.get('post_id')
+
         post = get_object_or_404(Post, id=post_id)
+
         form = CommentsCreateForm(request.POST)
 
         if form.is_valid():
             comment = form.save(commit=False)
             comment.post = post
-            comment.author = request.user.profile
+            comment.author = request.user
             comment.save()
 
-            return JsonResponse({'succes': 'Comment added successfully'})
-        else:
-            return JsonResponse({'error': 'Invalid form submission'}, status=400)
+        return redirect(self.get_success_url())
